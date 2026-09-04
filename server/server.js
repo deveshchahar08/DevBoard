15fcc65f85f7372aa04d5c9f5328946a119aa836
Devesh Chahar <tera-email@gmail.com>
2026-09-04 17:27:40 +0530

    chore: connect mongodb

---
+++ server/server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const connectDB = require("./config/db")
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res)=> {
+    res.json({
+        meessage: "DevBoard API is running"
+    });
+});
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+});
