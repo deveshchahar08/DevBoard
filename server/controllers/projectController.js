24bdd1a9eaa13060b1b01b3bcce05b97a2b031f1
Devesh Chahar <tera-email@gmail.com>
2026-09-15 03:21:24 +0530

    feat: compete project crud

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -26,6 +26,105 @@ const createProject = async(req, res) =>{
     }
 }
 
+const getProjects = async (req, res) =>{
+    try{
+        const projects =await Project.find({
+            owner: req.user,
+        }).sort({createdAt: -1})
+
+        res.status(200).json(projects)
+    } catch(error) {
+        res.status(500).json({
+            message: "server error",
+            error: error.message,
+        })
+    }
+}
+
+const getProjectById = async (req,res)=> {
+    try{
+        const project = await Project.findOne({
+            _id: req.params.id,
+            owner: req.user
+        })
+
+        if(!project) {
+            return res.status(404).json({
+                message: "project not found"
+            })
+        }
+
+        res.status(200).json(project)
+    } catch(error) {
+        res.status(500).json({
+            message: "server error",
+            error: error.message
+        })
+    }
+}
+
+const updateProject = async(req, res) => {
+    try{
+        const {title, description, status, dueDate} = req.body
+
+        const project = await Project.findOne({
+            _id: req.params.id,
+            owner: req.user,
+        });
+
+        if(!project){
+            return res.status(404).json({
+                message: "project not found "
+            })
+        }
+
+        project.title = title ?? project.title;
+        project.description = description ?? project.description;
+        project.status = status ?? project.status;
+        project.dueDate = dueDate ?? project.dueDate;
+
+        await project.save();
+
+        res.status(200).json({
+            message: "project updated successfully",
+            project,
+        })
+    } catch(error) {
+        res.status(500).json({
+            message: "server error",
+            error: error.message
+        })
+    }
+}
+
+const deleteProject = async (req,res) => {
+    try{
+        const project = await Project.findOneAndDelete({
+            _id: req.params.id,
+            owner: req.user,
+        })
+
+        if(!project){
+           return res.status(404).json({
+                message: " project not found ",
+            })
+        }
+
+        res.status(200).json({
+            message: "project deleted successfully"
+        })
+    }catch (error){
+        res.status(500).json({
+            message: " server error",
+            error: error.message
+        })
+    }
+}
+
 module.exports = {
     createProject,
+    getProjects,
+    getProjectById,
+    updateProject,
+    deleteProject,
 }
--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -3,6 +3,10 @@ const express = require("express")
 
 const  {
     createProject,
+    getProjects,
+    getProjectById,
+    updateProject,
+    deleteProject,
 } = require ("../controllers/projectController")
 
 const protect = require('../middleware/authMiddleware')
@@ -10,5 +14,9 @@ const protect = require('../middleware/authMiddleware')
 const router = express.Router()
 
 router.post("/", protect, createProject)
+router.get("/", protect, getProjects)
+router.get("/:id", protect, getProjectById)
+router.put("/:id", protect, updateProject)
+router.delete("/:id", protect, deleteProject)
 
 module.exports = router
